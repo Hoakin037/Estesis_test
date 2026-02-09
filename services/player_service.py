3d4105608353c550ab29b881b0ded8b38bfeb457
Hoakin <Hoakin037@gmail.com>
2026-02-09 20:57:08 +0700

    fix: del unnecessary error handlers have been removed

--- services/player_service.py
+++ services/player_service.py
@@ -10,23 +10,18 @@ class PlayerService():
         self.session = session
 
     async def get_player(self, player: PlayerBase):
-        try:
-            result = await self.rep.get_player(player, self.session)
-            if result != None:
-                return result
-            else:
-                raise HTTPException(status_code=401, detail="Пользователь не найден!")
-        except Exception as e:
-            raise e
+        result = await self.rep.get_player(player, self.session)
+        if result != None:
+            return result
+        else:
+            raise HTTPException(status_code=401, detail="Пользователь не найден!")
+    
 
     async def add_player(self, player: PlayerCreate):
-        try:
-            if await self.rep.get_player_by_nickname(PlayerGetByNick(id=player.id, nickname=player.nickname), self.session) != None:
-                raise HTTPException(status_code=409, detail="Пользователь с таким никнеймом уже существует!")
-            await self.rep.create_player(player, self.session)
-        except Exception as e:
-            raise e
-        
+        if await self.rep.get_player_by_nickname(PlayerGetByNick(id=player.id, nickname=player.nickname), self.session) != None:
+            raise HTTPException(status_code=409, detail="Пользователь с таким никнеймом уже существует!")
+        await self.rep.create_player(player, self.session)
+       
     async def del_player(self, player: PlayerBase):
         await self.rep.del_player(player, self.session)
 
